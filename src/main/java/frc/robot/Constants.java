063857195eaf39f43c6245d5e54bc5c4690a7c3f
akshaisrin <77131573+akshaisrin@users.noreply.github.com>
2022-01-15 15:28:15 -0800

    Joystick Control for Shooter
    code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,29 +13,11 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    //deadzone for the joysticks before input is used
-    public static final double joystickDeadzone = 0.1;
 
-    //Proportional adjustment for gyro pid
-    public static final double pGyro = 0.01;
-    //Gyro rotation rate deadzone before snap is enabled
-    public static final double gyroDeadzone = 3;
+    public static final int kShooterPort = 0;
+    public static final double increment = 0.2;
 
-    //PID settings
-    public static final Gains defaultPID = new Gains(0.05, 0.00001, 0.7, 0.0, 0.0, -0.5, 0.5, 0);
-    
-    //maximum speed of the robot in m/s
-    public static final double maxSpeed = 6;
-    //maximum rotation of the robot in rads/sec
-    public static final double maxTurnOutput = Math.toRadians(5);
-    //minimum input required for robot to move
-    public static final double minInput = 0.1;
-    //distance of wheels from center of robot
-    public static final double wheelDisFromCenter = 0.5715;
 
-    //IDs of CAN objects
-    public static final int frontLeftSparkID = 1;
-    public static final int frontRightSparkID = 2;
-    public static final int backLeftSparkID = 3;
-    public static final int backRightSparkID = 4;
+
+
 }
