d0efe90bca7dcac57238afb60a1424ec85b9ef50
cwkadi25355 <shahaditya@outlook.com>
2022-01-19 20:15:41 -0800

    dude.... what

--- src/main/java/frc/robot/subsystems/intake.java
+++ src/main/java/frc/robot/subsystems/intake.java
@@ -1,6 +1,4 @@
-@@ -5,55 +5,31 @@
 package frc.robot.subsystems;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj.Compressor;
@@ -25,38 +23,27 @@ public class intake extends SubsystemBase {
   double current = phCompressor.getPressure(); //no idea what this does, probably an issue
   XboxController joystick = new XboxController(0);
   Joystick _joystick = new Joystick(0);
-  TalonFX intakeMotor = new TalonFX(0);
+  Talon intakeMotor = new Talon(0);
   //PIDSetConfiguration
   double RPM=0.0;
   boolean isXbox = true;
-  phCompressor.enableDigital();
 
-
-  exampleSolenoidPH.set(true);
   /** Creates a new Intake. */
-  public intake() {  }
+  public intake() {}
 
   @Override
     public void periodic() {
     // This method will be called once per scheduler run
-    if (//joystick thing whenever i can download) { //pushes forward
-      exampleSolenoidPH.toggle();
-    }
 
-    else {
-
-      double stick = _joystick.getRawAxis(2);
-      intakeMotor.set(ControlMode.Velocity, -stick);
-    if (//joystick thing whenever i can download) { //pulls back
-      exampleSolenoidPH.toggle();
-    }
     if(isXbox){
       if (joystick.getPOV() == 0) {
+        exampleSolenoidPH.toggle();
         if (RPM+Constants.increment<=7500) {
           setRPM(RPM+Constants.increment);
         }
       }
       if (joystick.getPOV() == 180) {
+        exampleSolenoidPH.toggle();
         if (RPM-Constants.increment>=0) {
           setRPM(RPM-Constants.increment);
         }
@@ -64,7 +51,6 @@ public class intake extends SubsystemBase {
     }
 
     else {
-
       double stick = _joystick.getRawAxis(2);
       intakeMotor.set(ControlMode.Velocity, -stick);
     }
