e28bedc2ddd46268434de86d17c8de2223f7fedc
cwkadi25355 <shahaditya@outlook.com>
2022-01-19 19:34:47 -0800

    Update intake.java

--- src/main/java/frc/robot/subsystems/intake.java
+++ src/main/java/frc/robot/subsystems/intake.java
@@ -16,13 +16,44 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 
 public class intake extends SubsystemBase {
   /** Creates a new intake. */
+  XboxController joystick = new XboxController(0);
+  Joystick _joystick = new Joystick(0);
+  Talon intakeMotor = new Talon(0);
+  //PIDSetConfiguration
+  double RPM=0.0;
+  boolean isXbox = true;
   public intake() {}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    if(isXbox){
+      if (joystick.getPOV() == 0) {
+        if (RPM+Constants.increment<=7500) {
+          setRPM(RPM+Constants.increment);
+        }
+      }
+      if (joystick.getPOV() == 180) {
+        if (RPM-Constants.increment>=0) {
+          setRPM(RPM-Constants.increment);
+        }
+      }
+    }
+
+    else {
+
+      double stick = _joystick.getRawAxis(2);
+      intakeMotor.set(ControlMode.Velocity, -stick);
+    }
+  }
+
+  public void setRPM(double RPM) {
+    intakeMotor.set(ControlMode.Velocity, RPM);
+  }
+
   }
 }
