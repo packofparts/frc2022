23d5f52991e2653dc6f311b3e9eb88c0fbebd40c
akshaisrin <77131573+akshaisrin@users.noreply.github.com>
2022-01-20 19:39:03 -0800

    Finished Shooter- Testing Required

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,49 +1,42 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-//test commment
 package frc.robot.subsystems;
 
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableValue;
-import edu.wpi.first.networktables.*;
-public class Lidar extends SubsystemBase {
+import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveSubsystem;
 
-  /** Creates a new Lidar. */ 
-  public Lidar() {
-    float Kp = -0.1f; //proportional increment to adjust heading
-    float min_command = 0.05f; // minimum increment to get response from drivebase
+public class Limelight extends SubsystemBase {
+  /** Creates a new limelight. */
+  PIDController pidController;
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry targetx = table.getEntry("tx");
+  double tx = targetx.getDouble(0);
+  DriveSubsystem driveBase = new DriveSubsystem();
+  XboxController joystick = new XboxController(0);
+  public Limelight() {
+    double min_command = 0.05;
 
+    
+    this.pidController = new PIDController(Constants.kpLime, Constants.kiLime, Constants.kdLime);
+    pidController.setSetpoint(0);
 
-  //Accesses the tx value from network table ( Horizontal offset)
-    std::shared_ptr<NetworkTable> table = NetworkTable::GetTable("limelight");
-    float tx = table.GetNumber("tx");
-
-    if (joystick->GetRawButton(9))
-    {
-      float heading_error = -tx;
-      float steering_adjust = 0.0f;
-      if (tx > 1.0)
-      {
-              steering_adjust = Kp*heading_error - min_command;
-      }
-      else if (tx < 1.0)
-      {
-        steering_adjust = Kp*heading_error + min_command;
-      }
-      left_command += steering_adjust;
-      right_command -= steering_adjust;
-    }
-
-
-
-
+    
   }
-
-  @Override
+  
   public void periodic() {
-    // This method will be called once per scheduler run
+    // Move outside if error
+    if (joystick.getRawButton(Constants.autoAlignButton)) {
+      double speed = pidController.calculate(this.tx);
+      driveBase.m_backLeftSpark.set(-speed);
+      driveBase.m_frontLeftSpark.set(-speed);
+      driveBase.m_backRightSpark.set(speed);
+      driveBase.m_frontRightSpark.set(speed);
+    }
+    
   }
 }
