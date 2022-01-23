663335b27b11dec8a53f9cab050aaed0b6d6bf3d
akshaisrin <77131573+akshaisrin@users.noreply.github.com>
2022-01-23 12:32:04 -0800

    Updated Index

--- src/main/java/frc/robot/commands/transferIndex.java
+++ src/main/java/frc/robot/commands/transferIndex.java
@@ -4,21 +4,43 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Index;
+import edu.wpi.first.wpilibj.Timer;
 public class transferIndex extends CommandBase {
   /** Creates a new transferIndex. */
-  public transferIndex() {
-    // Use addRequirements() here to declare subsystem dependencies.
+
+  Index index = new Index();
+  Timer timer;
+  private boolean finish = false;
+  public transferIndex(double indexSpeed, Index in) {
+    index = in;
+    timer = new Timer();
+    addRequirements(index);
+    Constants.indexSpeed = indexSpeed;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    timer.reset();
+    timer.start();
+    while (timer.get() < Constants.spinTime) {
+      index.indexMotor.set(ControlMode.Velocity, Constants.indexSpeed);
+    }
+    System.out.println("index complete");
+    finish = true;
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
@@ -27,6 +49,6 @@ public class transferIndex extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return finish;
   }
 }
