01186c18f01a738db333fa38f417be838666cd6e
akshaisrin <77131573+akshaisrin@users.noreply.github.com>
2022-01-23 12:29:44 -0800

    Updated PID Turn

--- src/main/java/frc/robot/commands/PIDTurn.java
+++ src/main/java/frc/robot/commands/PIDTurn.java
@@ -4,21 +4,36 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-
-public class turn180 extends CommandBase {
-  /** Creates a new turn180. */
-  public turn180() {
-    // Use addRequirements() here to declare subsystem dependencies.
+import frc.robot.subsystems.Drive;
+import frc.robot.Constants;
+public class PIDTurn extends CommandBase {
+  
+  Drive driveBase = new Drive();
+  PIDController pid = new PIDController(0, 0, 0);
+  public PIDTurn(Drive dt, double Degrees) {
+    addRequirements(driveBase);
+    driveBase = dt;
+    Constants.PIDTurnDegrees = Degrees;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+ // class varibale that initializes gyro
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    double speed = pid.calculate(driveBase.yaw(), Constants.PIDTurnDegrees);
+    driveBase.m_backLeftSpark.set(-speed);
+    driveBase.m_frontLeftSpark.set(-speed);
+    driveBase.m_backRightSpark.set(speed);
+    driveBase.m_frontRightSpark.set(speed);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
