6f361123299a2786ae2378025527df13ca0b45c1
akshaisrin <77131573+akshaisrin@users.noreply.github.com>
2022-01-24 18:57:54 -0800

    Index

--- src/main/java/frc/robot/subsystems/Index.java
+++ src/main/java/frc/robot/subsystems/Index.java
@@ -5,10 +5,41 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Joystick;
+
+
+
 
 public class Index extends SubsystemBase {
   /** Creates a new Index. */
-  public Index() {}
+
+  XboxController xbox = new XboxController(Constants.xboxPort);
+  Joystick _joystick = new Joystick(Constants.joystickPort);
+  public TalonFX indexMotor = new TalonFX(Constants.indexPort);
+  boolean isXbox = false;
+
+  public Index() {
+    if(isXbox){
+      if (xbox.getPOV() == 0) {
+        if (Constants.motorPower+Constants.increment<=7500) {
+          indexMotor.set(ControlMode.Velocity, Constants.motorPower+Constants.increment);
+         
+        }
+      }
+      if (xbox.getPOV() == 180) {
+        if (Constants.motorPower-Constants.increment>=0) {
+          indexMotor.set(ControlMode.Velocity, Constants.motorPower-Constants.increment);
+          
+        }
+      }
+    }
+
+  }
 
   @Override
   public void periodic() {
