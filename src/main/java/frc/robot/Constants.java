7a513ebe54a8998b8dbaeb7468e101c880e43eca
akshaisrin <77131573+akshaisrin@users.noreply.github.com>
2022-01-26 19:33:54 -0800

    Code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -57,6 +57,7 @@ public final class Constants {
     //PID settings
     public static final Gains velocityPID = new Gains(0.0002, 0.0000005, 0.0, 0.0, 0.0, -1, 1, 0);
     public static Gains defaultPID = new Gains(0.2, 0.0, 0.0, 0.0, 0.0, -1, 1, 1);
+    public static double originalYaw;
     public static final double[] turnPID = new double[] {0.009, 0.0, 0.002};
     public static final double[] ratePID = new double[] {0.15, 0.008, 0.0};
     //maximum speed of the robot in m/s
