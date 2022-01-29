4f59af5c9775677734beeca169eac7f14b957285
akshaisrin <77131573+akshaisrin@users.noreply.github.com>
2022-01-29 11:54:24 -0800

    Finished Intake auto

--- src/main/java/frc/robot/commands/IntakeBall.java
+++ src/main/java/frc/robot/commands/IntakeBall.java
@@ -7,6 +7,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.IntakeSubsystem;
+
 public class IntakeBall extends CommandBase {
   /** Creates a new IntakeBall. */
   IntakeSubsystem intake = new IntakeSubsystem();
@@ -20,19 +21,19 @@ public class IntakeBall extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    
+    intake.setSolenoidPH(true);
+    intake.toggleMatics();
+    intake.IntakeBall();
+    intake.setSolenoidPH(false);
+    intake.toggleMatics();
+
 
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    intake.setSolenoidPH(true);
-    intake.toggleMatics();
-    intake.IntakeBall();
-    intake.OuttakeBall();
-    intake.setSolenoidPH(false);
-
+    
     
   }
 
